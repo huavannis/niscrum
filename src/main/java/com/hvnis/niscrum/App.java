c4531019a6b5e9f9c31f78acf45280afee0735c4
hvnis <huavannis@gmail.com>
2019-03-16 15:35:07 +0700

    create project

---
+++ src/main/java/com/hvnis/niscrum/App.java
@@ -0,0 +1,13 @@
+package com.hvnis.niscrum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
